c3fcd1cb7bdf3d0c091a65423269c8a4a7091cda
Davi-Possa <daviguimaraespossa@gmail.com>
2025-07-16 20:06:22 -0300

    HUD [feat/fix] - Adiciona envio de dados e arruma conflito

--- iris/web_ui/src/App.tsx
+++ iris/web_ui/src/App.tsx
@@ -1,20 +1,23 @@
+import { useState } from 'react';
+import { DataView } from './components/DataView';
+import { FieldView } from './components/FieldView';
+import { MenuBar } from './components/TopMenuBar';
+import { DataViewAll } from './components/DataViewAll';
 
-import { DataView } from "./components/DataView";
-import { FieldView } from "./components/FieldView";
-import { MenuBar } from "./components/TopMenuBar";
+export type SoftwareOption = 'ia' | 'gc' | 'vision' | 'tartarus' | 'caronte';
 
-function App() {
+export default function App() {
+  const [selectedSoftware, setSelectedSoftware] = useState<SoftwareOption | null>(null);
+  const [reading, setReading] = useState(false); // 🔁 Controle global da leitura
+
   return (
-    <>
-      <div className="bg-[#311A52] h-screen overflow-hidden">
-        <MenuBar />
-        <div className="flex flex-col lg:flex-row lg:justify-center gap-4 p-4">
-          <FieldView />
-          <DataView />
-        </div>
+    <div className="bg-[#311A52] h-screen overflow-hidden">
+      <MenuBar onSelectSoftware={setSelectedSoftware} />
+      <div className="flex flex-col lg:flex-row lg:justify-center gap-4 p-4">
+        <FieldView />
+        <DataView reading={reading} setReading={setReading} /> {/* passa controle */}
+        {selectedSoftware && <DataViewAll reading={reading} selected={selectedSoftware} />}
       </div>
-    </>
+    </div>
   );
 }
-
-export default App;
